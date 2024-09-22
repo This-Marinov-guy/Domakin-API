73b254bc52600eeb06d83fde88efc4a49e6d2f56
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-09-22 11:44:27 +0300

    up

--- app/Http/Controllers/Common/FeedbackController.php
+++ app/Http/Controllers/Common/FeedbackController.php
@@ -6,12 +6,19 @@ use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
-    
-    function fetchFeedbacks():ResponseJson
+
+    function fetchFeedbacks()
     {
-
-
+        // step 1: get language from query
+        // step 2: fetch all feedbacks that have approved = true and match language (if no language skip condition)
+        // step 3: return data
     }
 
-
+    function createFeedback()
+    {
+        // step 1: get body 
+        // step 2: validate data 
+        // step 3: create feedback
+        // step 4: return status true
+    }
 }
