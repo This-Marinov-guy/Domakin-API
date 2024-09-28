0a9790a19110ae4c7ebda3af37a58534ef73ceab
Todor <todor.tonev@gmail.com>
2024-09-28 22:49:42 +0200

    Made routes, controller and model. Haven't touched the logic yet. Need to make a Viewings Table

---
+++ app/Http/Controllers/Common/ViewingController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Common;
+
+use App\Classes\ApiResponseClass;
+use App\Http\Controllers\Controller;
+use App\Models\Viewing;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Feedback;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ViewingController extends Controller
+{
+    public function list(): JsonResponse
+    {
+        $viewings = Viewing::all();
+        return ApiResponseClass::sendSuccess($viewings);
+    }
+
+    public function details()
+    {
+        //
+    }
+    
+    public function create()
+    {
+        //
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Common\FeedbackController;
+use App\Http\Controllers\Common\ViewingController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::prefix('common')->group(function () {
     Route::get('/list-feedbacks', [FeedbackController::class, 'list']);
     Route::post('/create-feedback', [FeedbackController::class, 'create']);
+
+    Route::get('/viewing/list', [ViewingController::class, 'list']);
+    Route::get('/viewing/details/{id}', [ViewingController::class, 'details']);
+    Route::post('/viewing/create', [ViewingController::class, 'create']);
 });
