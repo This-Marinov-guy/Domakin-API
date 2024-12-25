605afa68206e9f84581548ee014a6967f6044886
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-12-25 23:48:28 +0200

    add property

---
+++ app/Models/Property.php
@@ -0,0 +1,56 @@
+<?php
+
+// app/Models/Property.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    protected $fillable = [
+        'personal_data',
+        'property_data'
+    ];
+
+    protected $casts = [
+        'personal_data' => 'array',
+        'property_data' => 'array'
+    ];
+
+    // Helper method to set data
+    public function setData(array $data)
+    {
+        $this->personal_data = $data['personalData'] ?? [];
+        $this->property_data = $data['propertyData'] ?? [];
+        return $this;
+    }
+
+    public function getFormattedData()
+    {
+        return [
+            'personalData' => $this->personal_data,
+            'propertyData' => $this->property_data
+        ];
+    }
+
+    public static function rules(): array
+    {
+        return [
+            'personalData.name' => 'required|string',
+            'personalData.surname' => 'required|string',
+            'personalData.email' => 'required|email',
+            'personalData.phone' => 'required|string',
+
+            'propertyData.city' => 'required|string',
+            'propertyData.address' => 'required|string',
+            'propertyData.size' => 'required|string',
+            'propertyData.period' => 'required|array',
+            'propertyData.rent' => 'required|array',
+            'propertyData.bills' => 'required|array',
+            'propertyData.flatmates' => 'required|array',
+            'propertyData.registration' => 'required|string',
+            'propertyData.description' => 'required|array',
+            'propertyData.images' => 'nullable|array',
+        ];
+    }
+}
---
+++ database/migrations/2024_12_25_224109_create_properties_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->json('personal_data');
+            $table->json('property_data');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
