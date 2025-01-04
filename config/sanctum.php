279b0d728e94a4672dc492e1e62b6093a2b62e86
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-01-04 23:53:07 +0200

    fixes

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,12 +15,11 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
-    ))),
+    'stateful' => [
+        env('FRONTEND_URL', 'localhost:3000'),
+        'release.domakin.nl',
+        'domakin.nl',
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +77,7 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        // 'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
 ];
