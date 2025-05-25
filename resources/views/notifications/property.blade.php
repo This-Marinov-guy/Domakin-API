9e8750e67e98cf3744ca1399907e923bffa484dd
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-05-25 20:23:14 +0300

    feat: add fallback values for personal and property data in notification template

--- resources/views/notifications/property.blade.php
+++ resources/views/notifications/property.blade.php
@@ -9,16 +9,16 @@
     <h1>New Property Upload</h1>
 
     <h2>Client Information</h2>
-    <p><strong>Name:</strong> {{ $data['personalData']['name'] }}</p>
-    <p><strong>Surname:</strong> {{ $data['personalData']['surname'] }}</p>
-    <p><strong>Email:</strong> {{ $data['personalData']['email'] }}</p>
-    <p><strong>Phone:</strong> {{ $data['personalData']['phone'] }}</p>
+    <p><strong>Name:</strong> {{ $data['personalData']['name'] ?? 'N/A' }}</p>
+    <p><strong>Surname:</strong> {{ $data['personalData']['surname'] ?? 'N/A' }}</p>
+    <p><strong>Email:</strong> {{ $data['personalData']['email'] ?? 'N/A' }}</p>
+    <p><strong>Phone:</strong> {{ $data['personalData']['phone'] ?? 'N/A' }}</p>
 
     <br />
 
     <h2>Property Information</h2>
-    <p><strong>City:</strong> {{ $data['propertyData']['city'] }}</p>
-    <p><strong>Address:</strong> {{ $data['propertyData']['address'] }}</p>
+    <p><strong>City:</strong> {{ $data['propertyData']['city'] ?? 'N/A' }}</p>
+    <p><strong>Address:</strong> {{ $data['propertyData']['address'] ?? 'N/A' }}</p>
     <!-- <p><strong>Size:</strong> {{ $data['propertyData']['size'] }}</p>
 
     <p><strong>Rent:</strong> {{ $data['propertyData']['rent'] }}</p>
