a189d310a96da0bd88e36233cf839da0bb3dc1ca
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2026-03-01 14:00:19 +0200

    update

--- app/Http/Middleware/VerifyWebhookSecret.php
+++ app/Http/Middleware/VerifyWebhookSecret.php
@@ -5,12 +5,15 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class VerifyWebhookSecret
 {
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->header('X-Webhook-Secret') !== config('supabase.webhook_secret')) {
+            Log::warning('Unauthorized webhook request', ['request' => $request->all()]);
+
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
